2a2e50ef2a8eb15acee9708321d9992cf47bb7f2
Antti Immonen <antti.h.immonen@aalto.fi>
2022-11-28 22:58:10 +0200

    Add test ofr total price with 2 products

--- viikko4/tdd-ostoskori/src/ostoskori.py
+++ viikko4/tdd-ostoskori/src/ostoskori.py
@@ -3,28 +3,28 @@ from ostos import Ostos
 
 class Ostoskori:
     def __init__(self):
-        self.ostokset = []
+        self.kori = []
         # ostoskori tallettaa Ostos-oliota, yhden per korissa oleva Tuote
 
     def tavaroita_korissa(self):
         # kertoo korissa olevien tavaroiden lukumäärän
         # eli jos koriin lisätty 2 kpl tuotetta "maito", tulee metodin palauttaa 2 
         # samoin jos korissa on 1 kpl tuotetta "maito" ja 1 kpl tuotetta "juusto", tulee metodin palauttaa 2 
-        return len(self.ostokset)
+        return len(self.kori)
 
     def hinta(self):
         sum = 0
-        for e in self.ostokset:
+        for e in self.kori:
             sum += e._saldo*e._hinta
         return sum
         # kertoo korissa olevien ostosten yhteenlasketun hinnan
 
     def lisaa_tuote(self, lisattava: Tuote):
-        if lisattava in self.ostokset:
-            self.ostokset[lisattava].saldo += 1
+        if lisattava in self.kori:
+            self.kori[lisattava]._saldo += 1
         else:
             lisattava._saldo = 1
-            self.ostokset.append(lisattava)
+            self.kori.append(lisattava)
 
     def poista_tuote(self, poistettava: Tuote):
         # poistaa tuotteen
@@ -35,6 +35,6 @@ class Ostoskori:
         # tyhjentää ostoskorin
 
     def ostokset(self):
-        pass
+        return self.kori
         # palauttaa listan jossa on korissa olevat ostos-oliot
         # kukin ostos-olio siis kertoo mistä tuotteesta on kyse JA kuinka monta kappaletta kyseistä tuotetta korissa on
